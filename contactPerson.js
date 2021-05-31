95b9f1f6d7817fbd68feb4e1d6728979a7372569
Pooja <pooja.14206@gmail.com>
2021-05-31 14:24:54 +0530

    added a file that create a addressbook contact

--- contactPerson.js
+++ contactPerson.js
@@ -1,22 +1,22 @@
-// class contactPerson{
+class contactPerson{
 
-//     firstName;
-//     lastName;
-//     address;
-//     city;
-//     state;
-//     zip;
-//     phoneNumber;
-//     email;
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
 
-//     contactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email){
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.address = address;
-//         this.city = city;
-//         this.state = state;
-//         this.zip = zip;
-//         this.phoneNumber = phoneNumber;
-//         this.email = email;
-//     }
-// }
+    contactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
